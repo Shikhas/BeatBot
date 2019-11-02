e65b550509faf4dc83da6f8d4bde3a5537f36aec
Shikhas <shikhasaxena7feb@gmail.com>
2019-11-02 16:46:42 -0500

    predict event function

--- vandydj/webb/views.py
+++ vandydj/webb/views.py
@@ -1,9 +1,18 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-
+from fastai import *
+from fastai.vision import *
 
 class UploadImageView(TemplateView):
     template_name = 'webb/image_upload.html'
 
     def post(self, request, *args, **kwargs):
         return HttpResponse(content=b'Hello this would display the inference from the model')
+
+    def get_classified_prob(image):
+        learn = load_learner(path=str(base_path / 'cohort_models/'), file='childproof_electrical_chicago.pkl')
+        prediction = learn.predict(image)
+        label = prediction[0]
+        # print(label)
+        prob = max(prediction[2])
+        return prob, label
